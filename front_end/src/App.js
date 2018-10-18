e2a96048244d0c5ee767a23b98b35bc875c81c86
john <johncusackccm@gmail.com>
2018-10-17 20:58:39 -0400

    upgraded the migrations for the new fields that we added.

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import Owners from './components/Owner/Owners'
+
+const API = 'http://127.0.0.1:5000/api/v1/';
+const DEFAULT_QUERY = 'owner'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      owners: [],
+      isLoading: false,
+      error: null,
+    };
+  }
+
+  getOwners() {
+    this.setState({ isLoading: true });
+
+    axios.get(API + DEFAULT_QUERY)
+      .then(result => this.setState({
+        owners: result.data.owners,
+        isLoading: false
+      }))
+      .catch(error => this.setState({
+        error,
+        isLoading: false
+      }));
+  }
+
+
   render() {
+    const { owners, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <button onClick={this.getOwners}></button>
+        <div>
+          {owners.map(owners =>{
+            return <Owners
+              email={owners.email}
+            />
+          })}
+        </div>
       </div>
     );
   }
